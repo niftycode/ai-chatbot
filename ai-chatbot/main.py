d64e6f6b28b4e574bdd78f804c9971dbfab25a2a
Bodo Schönfeld <bodo.schoenfeld@niftycode.de>
2023-11-14 14:01:36 +0100

    update main window

--- ai-chatbot/main.py
+++ ai-chatbot/main.py
@@ -14,35 +14,49 @@ import setup_ai as chatbot
 from tkinter import ttk
 
 
-class MainWindow(tk.Tk):
+class MainWindow():
     def __init__(self):
-        super().__init__()
+        self.window = tk.Tk()
+        self.window.title("AI Chatbot")
 
-        self.title("AI Chatbot")
-        self.geometry("800x600")
+        window_width = 600
+        window_height = 400
+
+        # Get the screen dimension
+        screen_width = self.window.winfo_screenwidth()
+        screen_height = self.window.winfo_screenheight()
+
+        # Find the center point
+        center_x = int(screen_width / 2 - window_width / 2)
+        center_y = int(screen_height / 2 - window_height / 2)
+
+        # Set the position of the window to the center of the screen
+        self.window.geometry(f"{window_width}x{window_height}+{center_x}+{center_y}")
+
+        # Window is not resizable
+        self.window.resizable(False, False)
 
         # Create a frame
         # All widgets will be part of this frame.
-        self.content = ttk.Frame()
+        self.content = ttk.Frame(self.window, padding=(10, 10, 10, 10))
 
         # Add widgets
-        self.text_area = tk.Text(self, bg="white")
+        self.text_area = tk.Text(self.content, bg="white")
         # self.text_area.pack()
 
-        self.input_field = ttk.Entry(self, width=100)
+        self.input_field = ttk.Entry(self.content)
         # self.input_field.pack()
 
-        self.chat_button = ttk.Button(self, text="Chat", command=lambda: self.send_message())
-        self.quit_button = ttk.Button(self, text="Quit", command=lambda: self.quit_program())
+        self.chat_button = ttk.Button(self.content, text="Chat", command=lambda: self.send_message())
+        self.quit_button = ttk.Button(self.content, text="Quit", command=lambda: self.quit_program())
         # self.send_button.pack()
 
         # Define a grid (3 columns, 3 rows)
-        self.text_area.grid(column=0, row=1, columnspan=1, rowspan=3)
-        self.input_field.grid(column=0, row=2, columnspan=1, rowspan=3)
-        self.chat_button.grid(column=0, row=3, columnspan=1, rowspan=1)
-        self.quit_button.grid(column=2, row=3, columnspan=1, rowspan=1)
-
-
+        self.content.grid(column=0, row=0, sticky="nsew")
+        self.text_area.grid(column=0, row=1, columnspan=3, rowspan=1, sticky="nsew")
+        self.input_field.grid(column=0, row=2, columnspan=3, rowspan=1, sticky="ew")
+        self.chat_button.grid(column=0, row=3, columnspan=1, rowspan=1, sticky="w")
+        self.quit_button.grid(column=2, row=3, columnspan=1, rowspan=1, sticky="e")
 
     def send_message(self) -> None:
         """
@@ -66,8 +80,11 @@ class MainWindow(tk.Tk):
         self.text_area.insert("end", f"Chatbot: {response}\n")
         self.text_area.insert("end", "\n \n")  # Zwei Leerzeilen für bessere Lesbarkeit
 
+    def mainloop(self):
+        self.window.mainloop()
 
 # input_field.bind_all("<Return>", send_message)
+
 
 if __name__ == "__main__":
     main_window = MainWindow()
