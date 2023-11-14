a910587099ae3cc17aad9df4753e5bc2179a7d3f
Bodo Schönfeld <bodo.schoenfeld@niftycode.de>
2023-11-14 11:53:20 +0100

    add widgets

--- ai-chatbot/main.py
+++ ai-chatbot/main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 """
-Description goes here...
+Tkinter GUI
 Version: 1.0
-Python 3.11
+Python 3.12
 Date created: November 8th, 2023
-Date modified: -
+Date modified: November 14th, 2023
 """
 
 import tkinter as tk
@@ -14,45 +14,61 @@ import setup_ai as chatbot
 from tkinter import ttk
 
 
+class MainWindow(tk.Tk):
+    def __init__(self):
+        super().__init__()
 
-root = tk.Tk()
-root.title("AI Chatbot")
-root.geometry("800x600")
+        self.title("AI Chatbot")
+        self.geometry("800x600")
+
+        # Create a frame
+        # All widgets will be part of this frame.
+        self.content = ttk.Frame()
+
+        # Add widgets
+        self.text_area = tk.Text(self, bg="white")
+        # self.text_area.pack()
+
+        self.input_field = ttk.Entry(self, width=100)
+        # self.input_field.pack()
+
+        self.chat_button = ttk.Button(self, text="Chat", command=lambda: self.send_message())
+        self.quit_button = ttk.Button(self, text="Quit", command=lambda: self.quit_program())
+        # self.send_button.pack()
+
+        # Define a grid (3 columns, 3 rows)
+        self.text_area.grid(column=0, row=1, columnspan=1, rowspan=3)
+        self.input_field.grid(column=0, row=2, columnspan=1, rowspan=3)
+        self.chat_button.grid(column=0, row=3, columnspan=1, rowspan=1)
+        self.quit_button.grid(column=2, row=3, columnspan=1, rowspan=1)
 
 
-text_area = tk.Text(root, bg="white")
-text_area.pack()
 
-input_field = ttk.Entry(root, width=100)
-input_field.pack()
+    def send_message(self) -> None:
+        """
+        Get input from the user, fetch answers and show them in a text field.
+        """
 
-send_button = ttk.Button(root, text="Chat!", command=lambda: send_message())
-send_button.pack()
+        # Get input and delete the input field
+        user_input = self.input_field.get()
+        self.input_field.delete(0, "end")
 
+        # Show the input in the text field
+        self.text_area.insert("end", f"You: {user_input}\n")
+        self.text_area.insert("end", "\n")  # Leerzeile für bessere Lesbarkeit
+        chatbot.collect_input(user_input)
 
-def send_message() -> None:
-    """
-    Get input from the user, fetch answers and show them in a text field.
-    """
+        # Fetch the answer
+        response = chatbot.get_completion_from_messages(chatbot.context)
+        chatbot.collect_responses(response)
 
-    # Get input and delete the input field
-    user_input = input_field.get()
-    input_field.delete(0, "end")
-
-    # Show the input in the text field
-    text_area.insert("end", f"You: {user_input}\n")
-    text_area.insert("end", "\n")  # Leerzeile für bessere Lesbarkeit
-    chatbot.collect_input(user_input)
-
-    # Fetch the answer
-    response = chatbot.get_completion_from_messages(chatbot.context)
-    chatbot.collect_responses(response)
-
-    # Show the answer in the text field
-    text_area.insert("end", f"Chatbot: {response}\n")
-    text_area.insert("end", "\n \n")  # Zwei Leerzeilen für bessere Lesbarkeit
+        # Show the answer in the text field
+        self.text_area.insert("end", f"Chatbot: {response}\n")
+        self.text_area.insert("end", "\n \n")  # Zwei Leerzeilen für bessere Lesbarkeit
 
 
 # input_field.bind_all("<Return>", send_message)
 
-root.mainloop()
+if __name__ == "__main__":
+    main_window = MainWindow()
+    main_window.mainloop()
